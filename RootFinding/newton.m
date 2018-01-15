33e19e34194f905b7f6a62bf5b0d7e1b11111707
Christopher Roy <christopher.roy@mail.mcgill.ca>
2018-01-15 15:41:38 -0500

    Cleaned up scripts and improved comments

--- RootFinding/newton.m
+++ RootFinding/newton.m
@@ -1,8 +1,17 @@
 function r = newton(f, f_p, a, b, eps, N)
 %This function finds a root to arbitrary precision using the
 %newton's method
+%   f:    function
+%   f_p:  derivative of function 
+%   a:    starting point of interval
+%   b:    end point of interval
+%   eps:  tolerance
+%   N:    iteration limit
 
+%r_0: initial/prev guess
 r_0 = (a+b)/2;
+
+% r: current guess
 r = r_0 - (f(r_0))/(f_p(r_0));
 
 counter = 1;
@@ -10,6 +19,6 @@ while ((abs(r -r_0) > eps) && (N > counter))
     r_0 = r;
     r = r_0 - (f(r_0))/(f_p(r_0));
     counter = counter+1;
+    
 end 
-disp(r)
-r = counter;
+
